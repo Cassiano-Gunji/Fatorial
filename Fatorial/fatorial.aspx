b7dab654a6707f9c5279cf986be7daf763c44879
Cassiano-Gunji <cassiano.gunji@gmail.com>
2021-10-25 10:03:36 -0300

    Add project files.

---
+++ Fatorial/fatorial.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="fatorial.aspx.cs" Inherits="Fatorial.fatorial" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Fatorial</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Cálculo de fatorial de um número inteiro usando algoritmo recursivo</h1>
+            Digite um número inteiro: 
+            <asp:TextBox ID="txtNumero" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Fatorial" />
+            <br />
+            <asp:Label ID="lblFatorial" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Fatorial/fatorial.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Fatorial
+{
+    public partial class fatorial : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(txtNumero.Text);
+            int fatorial = fat(n);
+            lblFatorial.Text = n.ToString() + "! = " + fatorial.ToString();
+        }
+        private int fat(int n)
+        {
+            int fatorial = 1;
+
+            if (n > 1)
+            {
+                fatorial = n * fat(n - 1);
+            }
+
+            return fatorial;
+        }
+    }
+}
